8027874aea5135acb893b3c9bbca02a3c31b54dc
Yuriy Solovyov <dev@solovyov.de>
2022-01-18 22:20:26 +0100

    added hikari and postgres jdbc driver

---
+++ src/main/java/de/solovyov/demo/datasource/PostgresDataSource.java
@@ -0,0 +1,19 @@
+package de.solovyov.demo.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresDataSource {
+
+    @Bean
+    @ConfigurationProperties
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(Hikari);
+    }
+}
