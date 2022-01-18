48de507535e1f8fec04f153d7b2b7e126e364b5a
Yuriy Solovyov <dev@solovyov.de>
2022-01-18 23:39:52 +0100

    add retrieve list of people from database. tested

--- src/main/java/de/solovyov/demo/dao/PersonDataAccessService.java
+++ src/main/java/de/solovyov/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package de.solovyov.demo.dao;
 
 import de.solovyov.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,7 +26,12 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return this.jdbcTemplate.query(sql, (rs, rowNum) -> {
+            UUID id = UUID.fromString(rs.getString("id"));
+            String name = rs.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
